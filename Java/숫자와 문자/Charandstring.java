a0b668358a950dbc47e0574e21bc2c174809b323
choi-dongmin <57117748+choi-dongmin@users.noreply.github.com>
2019-10-30 17:17:03 +0100

    Add files via upload

---
+++ Java/숫자와 문자/Charandstring.java
@@ -0,0 +1,9 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public final class Charandstring {
+
+	public static void main(String[] args) {
+		System.out.println("최동민이 말하길\n\"나는 파리에 있다\"");
+	}
+
+}
---
+++ Java/숫자와 문자/Number.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class Number {
+
+	public static void main(String[] args) {
+	System.out.println(3.0/2.0);
+
+	}
+
+}
